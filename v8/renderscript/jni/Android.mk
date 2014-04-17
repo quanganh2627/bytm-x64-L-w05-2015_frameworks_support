2d47e9321ac716107176e05d4cdc8b6ec8da2ee6
zwang92 <zhenghua.wang@intel.com>
2014-04-17 14:13:41 +0800

    rs: resolve build conflicts
    BZ: 186327
    
    Google release Intel x86 porting GMS package
    gms-oem-kkmr1-4.4.2-x86-signed-r2-20140401-1031837-PREVIEW
    Using FLAG_GMS_AVAILABLE to differentiate Internal release and external release
    
    Change-Id: If380d00346858d12e1935d0c62952c16940869fa
    Category: feature differentiation
    Domain: AOSP.GMS-Integration
    Origin: 3rd-party Google
    Upstream-Candidate: no, not-aosp
    Signed-off-by: zwang92 <zhenghua.wang@intel.com>

--- v8/renderscript/jni/Android.mk
+++ v8/renderscript/jni/Android.mk
@@ -5,7 +5,7 @@ LOCAL_SRC_FILES:= \
     android_renderscript_RenderScript.cpp
 
 LOCAL_SHARED_LIBRARIES := \
-        libRSSupport_old \
+        libRSSupport \
         libjnigraphics
 
 LOCAL_STATIC_LIBRARIES := \
@@ -23,9 +23,13 @@ LOCAL_CFLAGS +=
 
 LOCAL_LDLIBS := -lpthread
 LOCAL_ADDITIONAL_DEPENDENCIES := $(addprefix $(rs_generated_include_dir)/,rsgApiFuncDecl.h)
+ifeq ($(FLAG_GMS_AVAILABLE),yes)
 LOCAL_MODULE:= librsjni_old
+else
+LOCAL_MODULE:= librsjni
+endif
 LOCAL_ADDITIONAL_DEPENDENCIES += $(rs_generated_source)
 LOCAL_MODULE_TAGS := optional
-LOCAL_REQUIRED_MODULES := libRSSupport_old
+LOCAL_REQUIRED_MODULES := libRSSupport
 
 include $(BUILD_SHARED_LIBRARY)
